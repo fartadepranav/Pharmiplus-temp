3122463c801cf5f0843d1c22fd3d46a6687b8c96
Utkarsh Koppikar <47137566+utkarshkoppikar7@users.noreply.github.com>
2021-06-09 15:52:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const Product = require('./Models/Product');
+const cors = require('cors');
 const app = express();
 
 //Comment the below line if your not connecting to DB
 connectDB();
 
+app.use(cors());
 //Initialize middleware
 app.use(express.json({ extended: false })); // for application/json
 app.use(express.urlencoded({ extended: true })); //for forms
@@ -20,4 +23,14 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server started on PORT ${PORT}`));
 
+// Get all products from mongoDB
+app.get('/api/products', (req,res) => {
+    Product.find()
+      .then((result) => {
+          res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
